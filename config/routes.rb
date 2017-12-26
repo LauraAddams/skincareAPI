1f673f4ca63f5170657a148b73e19891a177d5fc
Laura Robertson <mslaura.mae@gmail.com>
2017-12-25 20:51:42 -0800

    Search route changed to product query for consistency

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :products, only: [:index, :show, :create]
-  get '/search', to: 'products#search'
+  get '/product', to: 'products#search'
 
   get '/ingredients', to: 'ingredients#index'
   get '/ingredient', to: 'ingredients#search'
