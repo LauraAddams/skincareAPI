e7f19e3e87290977d9ff0ea3df76dec2f3a9d15c
Laura Robertson <mslaura.mae@gmail.com>
2017-12-26 13:57:36 -0800

    Model validations updated for product, seeds updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ApplicationRecord
   validates :brand, presence: true
+  validates_format_of :brand, :with => /\A[^<>~!$;=?@`|{}]+\z/, :on => :create, :message => 'Contains an invalid symbol <>~!$;=?@`|{}'
   validates :name, presence: true
+  validates_format_of :name, :with => /\A[^<>~!$;=?@`|{}]+\z/, :on => :create, :message => 'Contains an invalid symbol <>~!$;=?@`|{}'
   validates :ingredients, presence: true
+  validates_format_of :ingredients, :with => /\A[^<>~!$;=?@`|{}]+\z/, :on => :create, :message => 'Contains an invalid symbol <>~!$;=?@`|{}'
 
   private
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -45,7 +45,7 @@ CSV_FILES.each do |file_name|
     end
   end
 
-  all_ingredients = all_ingredients.uniq
+  all_ingredients = all_ingredients.uniq.sort
 end
 
 all_ingredients.each do |i|
